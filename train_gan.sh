75c9abf89a6acdfc139d11d8c6a01f518d04bcfb
eduOS <fanle_2013@163.com>
2018-02-12 10:58:33 +0800

    decrease the learning rate further

--- train_gan.sh
+++ train_gan.sh
@@ -7,8 +7,9 @@ python  SumGan.py \
   --dis_vocab_file vocab \
   --single_pass False \
   --beam_size 4 \
-  --gan_lr 0.0001 \
-  --gan_lr 0.0001 \
+  --gan_lr 0.00005 \
+  --gen_lr 0.00005 \
+  --dis_lr 0.00005 \
   --vocab_type char \
   --dis_reward_ratio 1 \
   --rollout_num 12 \
